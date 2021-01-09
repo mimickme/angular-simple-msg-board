b66d2788d7357b36d0e229e5f8d01cf87882b8a6
mimickme <mimickme@gmail.com>
2021-01-08 21:26:29 -0500

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { TextEntry } from './textEntry';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
   inputTextControl = new FormControl('');
+  textEntries: TextEntry[];
+  textEntriesSubscription$: Observable<TextEntry[]>;
+
+  constructor() {
+  }
+  
+  ngOnInit() {
+    this.textEntries = [];
+  }
 
   enterText() {
-    console.log(this.inputTextControl.value);
+    this.textEntries.push(new TextEntry("", this.inputTextControl.value));
+    this.inputTextControl.setValue('');
   }
 }
---
+++ src/app/textEntry.ts
@@ -0,0 +1,9 @@
+export class TextEntry {
+  timeStamp;
+  textValue;
+
+  constructor(timeStamp, textValue) {
+    this.timeStamp = timeStamp;
+    this.textValue = textValue;
+  }
+}
